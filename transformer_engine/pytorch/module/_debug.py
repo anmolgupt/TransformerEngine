5b13e284e1d56e74387d7d1f6b250a9876ce7f0d
Anmol Gupta <anmolg@nvidia.com>
2024-01-19 11:50:15 -0800

    amax_x check for valid value

--- transformer_engine/pytorch/module/_debug.py
+++ transformer_engine/pytorch/module/_debug.py
@@ -16,12 +16,13 @@ def set_current_amax(x, x_fp8_format, fp8_meta_tensor, tensor_type):
     #print("Calling fp8_cast_transpose_fused")
     amax_x = torch.max(torch.abs(x)).float()
     #print("AMAX VAL: ", amax_x)
-    if x_fp8_format==tex.DType.kFloat8E5M2:
-        fp8_meta_scale = 57344. / amax_x
-    else:
-        fp8_meta_scale = 448. / amax_x
-    fp8_meta_scale_inv = 1. / fp8_meta_scale
-    fp8_meta_tensor.scale[tensor_type] = fp8_meta_scale
-    fp8_meta_tensor.scale_inv[tensor_type] = fp8_meta_scale_inv
+    if amax_x > 0.0 and torch.isfinite(amax_x)
+        if x_fp8_format==tex.DType.kFloat8E5M2:
+            fp8_meta_scale = 57344. / amax_x
+        else:
+            fp8_meta_scale = 448. / amax_x
+        fp8_meta_scale_inv = 1. / fp8_meta_scale
+        fp8_meta_tensor.scale[tensor_type] = fp8_meta_scale
+        fp8_meta_tensor.scale_inv[tensor_type] = fp8_meta_scale_inv
     #print("---Scale and Scale inv after update: ", fp8_meta_tensor.scale[tensor_type], fp8_meta_tensor.scale_inv[tensor_type])
     #print("Before Amax history: ", fp8_meta_tensor.amax_history[0][tensor_type])
