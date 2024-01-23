7b96ef736c9375bbdbff2ff7e56859e4bfd9070a
Anmol Gupta <14880251+anmolgupt@users.noreply.github.com>
2024-01-23 09:37:50 -0800

    changed the scale and inv assignment

--- transformer_engine/pytorch/module/_debug.py
+++ transformer_engine/pytorch/module/_debug.py
@@ -15,14 +15,16 @@ def clip_to_fp8(x, e5m2=False):
 def set_current_amax(x, x_fp8_format, fp8_meta_tensor, tensor_type):
     #print("Calling fp8_cast_transpose_fused")
     amax_x = torch.max(torch.abs(x)).float()
-    #print("AMAX VAL: ", amax_x)
+    #print("---Scale and Scale inv before update: ", fp8_meta_tensor.scale[tensor_type], fp8_meta_tensor.scale_inv[tensor_type])
+    #print("---AMAX VAL: ", amax_x)
+    #print("---Before Amax history: ",tensor_type, fp8_meta_tensor.amax_history)
     if amax_x > 0.0 and torch.isfinite(amax_x):
         if x_fp8_format==tex.DType.kFloat8E5M2:
             fp8_meta_scale = 57344. / amax_x
         else:
             fp8_meta_scale = 448. / amax_x
         fp8_meta_scale_inv = 1. / fp8_meta_scale
-        fp8_meta_tensor.scale[tensor_type] = fp8_meta_scale
-        fp8_meta_tensor.scale_inv[tensor_type] = fp8_meta_scale_inv
-    #print("---Scale and Scale inv after update: ", fp8_meta_tensor.scale[tensor_type], fp8_meta_tensor.scale_inv[tensor_type])
+        fp8_meta_tensor.scale[tensor_type].copy_(fp8_meta_scale)
+        fp8_meta_tensor.scale_inv[tensor_type].copy_(fp8_meta_scale_inv)
+        #print("---Scale and Scale inv after update: ", fp8_meta_tensor.scale[tensor_type], fp8_meta_tensor.scale_inv[tensor_type])
     #print("Before Amax history: ", fp8_meta_tensor.amax_history[0][tensor_type])
